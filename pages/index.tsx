031e48d735010e534ef8906925c9e75771f3d5a1
Kaio HSG <kaio12hsg@gmail.com>
2024-10-31 18:44:23 -0300

    Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,12 @@
-function Home() {
-    return <div>Home</div>
+import Link from "next/link";
+
+function Casa() {
+    return (
+        <div>
+            <h1>Casa</h1>
+            <a><Link href="/sobre">Sobre</Link></a>
+        </div>
+    )
 }
 
-export default Home
+export default Casa
---
+++ pages/sobre.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+function Sobre() {
+    return (
+        <div>
+            <h1>Sobre</h1>
+            <a><Link href="/">Casa</Link></a>
+        </div>
+    )
+}
+
+export default Sobre
