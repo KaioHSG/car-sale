bb35fb26520ceac6454f437d1336ee660da84355
Kaio HSG <kaio12hsg@gmail.com>
2024-11-07 21:59:35 -0300

    Update

--- pages/api/db.ts
+++ pages/api/db.ts
@@ -1,14 +1,20 @@
 import { Client } from 'pg';
 
+let sslConfig
+
+if (process.env.PRODUCTION === 'true') {
+    sslConfig = {
+        rejectUnauthorized: false,
+    }
+}
+
 const client = new Client({
     user: process.env.PG_USER,
     host: process.env.PG_HOST,
     database: process.env.PG_DATABASE,
     password: process.env.PG_PASSWORD,
     port: process.env.PG_PORT,
-    ssl: process.env.PRODUCTION === 'true' ? {
-        rejectUnauthorized: false, // Aceita qualquer certificado SSL em produção
-    } : false, // Não utiliza SSL em desenvolvimento
+    ssl: sslConfig,
 });
 
 client.connect();
